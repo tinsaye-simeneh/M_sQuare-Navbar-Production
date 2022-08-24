40c6236aabaf354b1361e07ed8220dabc5c9e207
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-08-25 01:20:18 +0300

    menu section details added

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,15 @@ export default function Navbar() {
   return (
     <div className="container mt-3 text-white rounded bg-black">
       <div className="row p-3">
-        <div className="col-md-4 bg-primary">
-            Logo Section
-        </div>
-        <div className="col-md-8 bg-secondary">
-            Menu Section
+        <div className="col-md-6 bg-primary">Logo Section</div>
+        <div className="col-md-6 bg-secondary">
+          <div className="row">
+            <div className="col-md-2 bg-dark text-center">Home</div>
+            <div className="col-md-2 bg-dark text-center">Home</div>
+            <div className="col-md-2 bg-dark text-center">Home</div>
+            <div className="col-md-2 bg-dark text-center">Home</div>
+            <a className="btn btn-primary col-md-3">Login</a>
+          </div>
         </div>
       </div>
     </div>
