e73c2123607610b29e7130a85a3579f8830ea245
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-08-25 03:23:45 +0300

    Account button position fixed

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,16 +2,18 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="container mt-3 text-white rounded bg-black">
-      <div className="row p-3">
-        <div className="col-md-6 bg-primary">Logo Section</div>
-        <div className="col-md-6 bg-secondary">
+    <div className="container mt-4 text-dark shadow-lg px-5 p-3 mx-auto mb-5 rounded">
+      <div className="row">
+        <div className="col-md-6">Logo Section</div>
+        <div className="col-md-6">
           <div className="row">
-            <div className="col-md-2 bg-dark text-center">Home</div>
-            <div className="col-md-2 bg-dark text-center">Home</div>
-            <div className="col-md-2 bg-dark text-center">Home</div>
-            <div className="col-md-2 bg-dark text-center">Home</div>
-            <a className="btn btn-primary col-md-3">Login</a>
+            <div className="col-md-2 text-center">Home</div>
+            <div className="col-md-3 text-center">About Us</div>
+            <div className="col-md-2 text-center">Services</div>
+            <div className="col-md-3 align-center text-center">Contact Us</div>
+            <button href="#account" className="border-0 text-white rounded col-md-2" style={{backgroundColor:'#E62B00'}}>
+              Account
+            </button>
           </div>
         </div>
       </div>
