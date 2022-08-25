15c1f899c5a898b8dac9e2ad5082cd4fb1d8ad84
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-08-25 21:07:34 +0300

    a link to the logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,21 +6,31 @@ export default function Navbar() {
     <div className="container mt-4 text-dark shadow-lg px-5 p-3 mx-auto mb-5 rounded">
       <div className="row">
         <div className="col-md-6">
-          <img src={Logo} alt="MsQuare Logo" />
+          <a href="/Home">
+            <img src={Logo} alt="MsQuare Logo" />
+          </a>
         </div>
         <div className="col-md-6">
           <div className="row">
             <div className="col-md-2 text-center">
-              <a href="/Home" className="text-dark text-decoration-none">Home</a>
+              <a href="/Home" className="text-dark text-decoration-none">
+                Home
+              </a>
             </div>
             <div className="col-md-3 text-center">
-              <a href="/About" className="text-dark text-decoration-none">About Us</a>
+              <a href="/About" className="text-dark text-decoration-none">
+                About Us
+              </a>
             </div>
             <div className="col-md-2 text-center">
-              <a href="/Service" className="text-dark text-decoration-none">Services</a>
+              <a href="/Service" className="text-dark text-decoration-none">
+                Services
+              </a>
             </div>
             <div className="col-md-3 align-center text-center">
-              <a href="/Contact" className="text-dark text-decoration-none">Contact Us</a>
+              <a href="/Contact" className="text-dark text-decoration-none">
+                Contact Us
+              </a>
             </div>
             <button
               href="#account"
